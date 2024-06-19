3c862a8ee8f74c193b0ff3ed87fb60fcfc6388c2
davidsat <reygun13@hotmail.fr>
2024-06-19 15:41:38 +0200

    MAJ cv

--- src/App.js
+++ src/App.js
@@ -64,38 +64,42 @@ return(
                   <i class="fa-solid fa-location-dot"></i>
                   <span className='ms-1'>Île-de-France</span>
                   <br></br>
-                  <a href="tel:+33612131415"className='mt-3'>
-                  <i class="fa-solid fa-mobile-screen-button"></i>
-                  <span className='ms-1'>+33 06 14 82 50 09</span>
-                  </a>
+                  {/* <a href="tel:+33612131415"className='mt-3'> */}
+                  <i class="fa-solid fa-mobile-screen-button my-2"></i>
+                  <span className='ms-1 my-2'>+33 06 14 82 50 09</span>
+                  {/* </a> */}
                   <br></br>
-                  <a href="mailto:david.sat@hotmail.fr"className='my-3'>
+                  {/* <a href="mailto:david.sat@hotmail.fr"className='my-3'> */}
                   <i className="fa-regular fa-envelope"></i>
                   <span className='ms-1'>david.sat@hotmail.fr</span>
-                  </a>
+                  {/* </a> */}
                   <br></br>
-                  <a href="https://github.com/reygun888">
-                  <i className="fa-brands fa-github"></i>
-                  <span className='ms-1'>https://github.com/reygun888</span>
-                  </a>
+                  {/* <a href="https://github.com/reygun888"> */}
+                  <i className="fa-brands fa-github my-2"></i>
+                  <span className='ms-1 my-2'>https://github.com/reygun888</span>
+                  {/* </a> */}
                   <br></br>
-                  <a href="https://reygun888.github.io/prosdd/"className='mt-3'>
+                  {/* <a href="https://reygun888.github.io/prosdd/"className='mt-3'> */}
                   <i class="fa-brands fa-internet-explorer"></i>
                   <span className='net ms-1'>https://reygun888.github.io/prosdd</span>
-                  </a>
+                  {/* </a> */}
                 </div>
               </li>
               <li>
               <div className='competence'>
-                <h5>EXP. TECHNO</h5>
+                <h5>LANGAGES DE PROGRAMMATION</h5>
               <div className='contact'>
-                <p>Langages de programmation</p>
                 <li>- HTML</li>
                 <li>- JavaScript</li>
                 <li>- PHP</li>
                 <li>- CSS</li>
-                <hr></hr>
-                <p>Framework et bibliothèque</p>
+              </div>
+              </div>
+              </li>
+              <li>
+              <div className='competence'>
+                <h5>FRAMEWORKS ET BIBLIOTHÈQUES</h5>
+              <div className='contact'>
                 <li>- Symfony</li>
                 <li>- Angular</li>
                 <li>- React</li>
@@ -162,88 +166,120 @@ return(
             <h4> PRÉSENTATION</h4>
             <div className='pre my-3'>
               <p>
-              Sortant d’une formation de développement Web et Mobile, je souhaiterais me perfectionner en mettant en exergue mes connaissances et étoffer mon savoir-faire par de nouvelles compétences.
+              Mes expériences m'ont permis de développer des qualités essentielles telles que la rigueur, la gestion du temps, et la capacité à travailler sous pression, atouts qui seront indéniablement bénéfiques dans le domaine du Dev.Web. De plus, ma certification en développement web et mobile témoigne de ma capacité à assimiler rapidement de nouvelles technologies et à m'adapter à des environnements techniques variés.
               </p>
             </div>
             <h4> PROJETS PERSONNEL</h4>
             <div className='pre my-3'>
-              <h3>•	Site vitrine pour ‘Sécurité d’Abord’</h3>
+              <h3>Site vitrine pour ‘Sécurité d’Abord’</h3>
+              <p>Développement d'un site vitrine pour une société de sécurité en respectant une maquette fournie et en assurant une visibilité multiplatforme. Le projet a impliqué la création de la structure HTML, la mise en forme avec CSS, l'optimisation du SEO et des performances, la réalisation d'un CI/CD et le déploiement du site. Cette expérience m'a permis de renforcer mes compétences en développement web, en intégration de maquette, en optimisation de performances et en déploiement continu.</p>
               <ul className='desc my-3'>
-                <li className='mb-4'> 
-                    <a href="https://reygun888.github.io/security/">
-                    <i class="fa-brands fa-internet-explorer"></i>
-                    <span> https://reygun888.github.io/security/</span>
-                    </a>
-                </li>
-                <li>-	Élaboration de la page HTML</li>
-                <li>-	Mise en forme du site</li>
-                <li>-	Rendre le site responsif sur divers supports</li>
-                <li>-	Optimiser le SEO et les performances de la page</li>
-                <li>- Réalisation d'un CI/CD</li>
-                <li>- Déploiement du site</li>
-                <li className='my-3'>
-                  <a href="https://github.com/reygun888/Security.git">
+              <div className='d-flex justify-content-around'>
+                <div> 
+                  <li>-	Élaboration de la page en HTML</li>
+                  <li>-	Mise en forme du site</li>
+                  <li>-	Rendre le site responsif sur divers supports</li>
+                </div>
+               <div>
+                  <li>-	Optimiser le SEO et les performances de la page</li>
+                  <li>- Réalisation d'un CI/CD</li>
+                  <li>- Déploiement du site</li></div>
+                </div>
+                <div className=' liens my-3 d-flex justify-content-center'>
+                <li className='mx-2'>
+                  {/* <a href="https://github.com/reygun888/Security.git"> */}
                   <i className="fa-brands fa-github"></i>
                   <span> https://github.com/reygun888/Security.git</span>
-                  </a>
+                  {/* </a> */}
                 </li>
-              </ul>
-              <hr className='sep'></hr>
-            </div>
-            <div className='pre my-3'>
-              <h3>•	Application web d’un gestionnaire de personnel pour ‘Ressources Formation’</h3>
-              <ul className='desc my-3'>
-                <li>-	Utilisation du Framework Symfony et des langages HTML, PHP, CSS, JavaScript, SQL</li>
-                <li>-	Conception du diagramme de classe et de l’User Case</li>
-                <li>-	Création d’un calendrier avec des fonctionnalités d’écoute et d’affichage</li>
-                <li>-	Mise en place de la base de données</li>
-                <li>-	Initialisation de différents CRUD</li>
-                <li className='my-3'> 
-                  <a href="https://github.com/reygun888/GestionRH.git">
-                  <i className="fa-brands fa-github"></i>
-                  <span> https://github.com/reygun888/GestionRH.git</span>
-                  </a>
-                </li>
-              </ul>
-              <hr className='sep'></hr>
-            </div>
-            <div className='pre my-3'>
-              <h3>•	Création d'un site pour afficher mon profil</h3>
-              <ul className='desc my-3'>
-                <li className='mb-4'> 
-                    <a href="https://reygun888.github.io/prosdd/">
+                <li className='mx-2'> 
+                    {/* <a href="https://reygun888.github.io/security/"> */}
                     <i class="fa-brands fa-internet-explorer"></i>
-                    <span> https://reygun888.github.io/prosdd/</span>
-                    </a>
+                    <span> https://reygun888.github.io/security/</span>
+                    {/* </a> */}
                 </li>
+                </div>
+              </ul>
+              <hr className='sep'></hr>
+            </div>
+            <div className='pre my-3'>
+              <h3>Création d'un site pour afficher mon profil</h3>
+              <p>Développement d'un site dynamique avec React pour présenter mon profil et mes projets. Utilisation de diverses API pour ajouter des fonctionnalités telles que des animations et un carrousel de projets. Le site a été rendu responsif pour une visibilité optimale sur différents supports et déployé via GitHub Pages. Cette expérience m'a permis de renforcer mes compétences en développement web avec React et en intégration d'API.</p>
+              <ul className='desc my-3'>
+              <div className='d-flex justify-content-around'>
+              <div> 
                 <li>-	Élaboration d'un site dynamique avec la bibliothèque 'REACT'</li>
                 <li>-	Utilisation de divers API (AOS, Swiper, ...)</li>
+              </div>
+              <div>
                 <li>-	Rendre le site responsif sur divers supports</li>
                 <li>-	Déploiement du site via GitHub</li>
-                <li className='my-3'>
-                  <a href="https://github.com/reygun888/prosdd.git">
+              </div>
+              </div>
+                <div className='liens my-3 d-flex justify-content-center'>
+                <li className='mx-2'> 
+                    {/* <a href="https://reygun888.github.io/prosdd/"> */}
+                    <i class="fa-brands fa-internet-explorer"></i>
+                    <span> https://reygun888.github.io/prosdd/</span>
+                    {/* </a> */}
+                </li>
+                <li className='mx-2'>
+                  {/* <a href="https://github.com/reygun888/prosdd.git"> */}
                   <i className="fa-brands fa-github"></i>
                   <span> https://github.com/reygun888/prosdd.git</span>
-                  </a>
+                  {/* </a> */}
                 </li>
+                </div>
               </ul>
             </div>
             <h4> EXPÉRIENCE PROFESSIONNELLES</h4>
             <div className='pre my-3'>
-              <h3>Conception et réalisation d’un site web pour l’ONG ‘Les Amis Du Monde’ sous Symfony, lors d’un stage en entreprise</h3>
+              <h3>Application web interne d’un gestionnaire de personnel pour ‘Ressources Formation’</h3>
+              <p>Cette expérience professionnelle m'a permis de renforcer mes compétences en développement web avec Symfony, en conception de base de données et en gestion de projet. J'ai également acquis une expérience pratique dans la création d'interfaces utilisateur et la mise en place de fonctionnalités complexes.</p>
               <ul className='desc my-3'>
-                <li>Référent : Paul Gaultier Toti – TMS-web – Tél. : 06 52 22 85 43</li>
-                <li className='my-3'>
-                  <a href='https://github.com/reygun888/ONG2.git'>
+              <div className='d-flex justify-content-around'>
+              <div> 
+                <li>-	Utilisation du Framework Symfony</li>
+                <li>- Utilisation des langages HTML, PHP, CSS, JavaScript, SQL	</li>
+                <li>-	Conception du diagramme de classe et de l’User Case</li>
+              </div>
+              <div>
+                <li>-	Mise en place de la base de données</li>
+                <li>-	Initialisation de différents CRUD</li>
+                <li>-	Création d’un calendrier avec des fonctionnalités d’écoute et d’affichage</li>
+              </div>
+              </div>
+                <li className='liens my-3'> 
+                  {/* <a href="https://github.com/reygun888/GestionRH.git"> */}
                   <i className="fa-brands fa-github"></i>
-                  <span> https://github.com/reygun888/ONG2.git</span>
-                  </a>
+                  <span> https://github.com/reygun888/GestionRH.git</span>
+                  {/* </a> */}
                 </li>
+              </ul>
+              <hr className='sep'></hr>
+            </div>
+            <div className='pre my-3'>
+              <h3>Conception et réalisation d’un site web pour l’ONG ‘Les Amis Du Monde’ sous Symfony, lors d’un stage en entreprise</h3>
+              <p>Développement d'un site web permettant la gestion des adhérents selon des rôles définis et la mise en place de fonctionnalités d'inscription, de don et de connexion. Le projet a impliqué la création d'un diagramme de classe pour modéliser les entités, la gestion des tâches selon leur difficulté et l'organisation des sprints. Un Dashboard a été mis en place pour permettre la modification du visuel du site. Cette expérience m'a permis de renforcer mes compétences en développement web avec Symfony, en gestion de projet agile et en conception de base de données.</p>
+              <ul className='desc my-3'>
+              <div className='d-flex justify-content-around'>
+              <div> 
+                <li>-	Conception du diagramme de classe</li>
+                <li>-	Création des formulaires</li>
+                <li>-	Mise en place de la gestion des rôles pour les adhérents</li>
+              </div>
+              <div>
+                <li>-	Organisation des tâches selon leur difficulté</li>
+                <li>- Planification des sprints pour le développement du site</li>
+                <li>-	Modification du visuel du site via un Dashboard</li>
+              </div>
+              </div>
+                <li className='liens my-3'>Référent : Paul Gaultier Toti – TMS-web – Tél. : 06 52 22 85 43</li>
             </ul>
             </div>
             <h4> FORMATION PROFESSIONNELLE</h4>
               <div className='pre my-3'>
-                <h3>Certification en Développement Web – Ressources Formation, 2024</h3>
+                <h3>Certification Professionnel Niv.5 en Développement Web et Web Mobile – Ressources Formation, 2024</h3>
               </div>
           </section>
         </main>
